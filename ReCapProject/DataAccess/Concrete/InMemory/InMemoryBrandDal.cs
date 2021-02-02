5a0432bdc686b710652b516ad95b19daa63a0059
drkamilacar <78367849+drkamilacar@users.noreply.github.com>
2021-02-02 16:53:06 +0300

    ReCapProject

---
+++ ReCapProject/DataAccess/Concrete/InMemory/InMemoryBrandDal.cs
@@ -0,0 +1,50 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryBrandDal : IBrandDal
+    {
+        List<Brand> _brands;
+        public InMemoryBrandDal()
+        {
+            _brands = new List<Brand>
+            {
+                new Brand{BrandId = 1, BrandName = "Opel"},
+                new Brand{BrandId = 2, BrandName = "BMW"},
+                new Brand{BrandId = 3, BrandName = "Fiat"}
+            };
+        }
+
+        public void Add(Brand brand)
+        {
+            _brands.Add(brand);
+        }
+
+        public void Delete(Brand brand)
+        {
+            Brand brandToDelete = _brands.SingleOrDefault(p => p.BrandId == brand.BrandId);
+            _brands.Remove(brandToDelete);
+        }
+
+        public List<Brand> GetAll()
+        {
+            return _brands;
+        }
+
+        public List<Brand> GetById(int brandId)
+        {
+            return _brands.Where(p => p.BrandId == brandId).ToList();
+        }
+
+        public void Update(Brand brand)
+        {
+            Brand brandToUpdate = _brands.SingleOrDefault(p => p.BrandId == brand.BrandId);
+            brandToUpdate.BrandName = brand.BrandName;
+        }
+    }
+}
---
+++ ReCapProject/DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,53 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _cars;
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car>
+            {
+                new Car{ Id = 1, BrandId = 1, ColorId = 1, DailyPrice = 150000, ModelYear = 2015, Description = "Opel Astra J"},
+                new Car{ Id = 2, BrandId = 2, ColorId = 2, DailyPrice = 180000, ModelYear = 2012, Description = "BMW 1.16i Comfort"},
+                new Car{ Id = 3, BrandId = 3, ColorId = 3, DailyPrice = 105000, ModelYear = 2011, Description = "Punto Evo Dynamic"}
+            };
+        }
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            Car carToDelete = _cars.SingleOrDefault(p => p.Id == car.Id);
+            _cars.Remove(carToDelete);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public List<Car> GetById(int carId)
+        {
+            return _cars.Where(p => p.Id == carId).ToList();
+        }
+
+        public void Update(Car car)
+        {
+            Car carToUpdate = _cars.SingleOrDefault(p => p.Id == car.Id);
+            carToUpdate.BrandId = car.BrandId;
+            carToUpdate.ColorId = car.ColorId;
+            carToUpdate.DailyPrice = car.DailyPrice;
+            carToUpdate.ModelYear = car.ModelYear;
+            carToUpdate.Description = car.Description;
+        }
+    }
+}
