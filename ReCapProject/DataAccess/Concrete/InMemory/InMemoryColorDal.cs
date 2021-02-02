94a6893b7ef622ad04e71520d7c7713f941c4e7a
drkamilacar <78367849+drkamilacar@users.noreply.github.com>
2021-02-02 16:49:04 +0300

    ReCapProject

---
+++ ReCapProject/DataAccess/Concrete/InMemory/InMemoryColorDal.cs
@@ -0,0 +1,50 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryColorDal : IColorDal
+    {
+        List<Color> _colors;
+        public InMemoryColorDal()
+        {
+            _colors = new List<Color>
+            {
+                new Color{ColorId = 1, ColorDescription = "Siyah"},
+                new Color{ColorId = 2, ColorDescription = "Beyaz"},
+                new Color{ColorId = 3, ColorDescription = "Gri"}
+            };
+        }
+
+        public void Add(Color color)
+        {
+            _colors.Add(color);
+        }
+
+        public void Delete(Color color)
+        {
+            Color colorToDelete = _colors.SingleOrDefault(p => p.ColorId == color.ColorId);
+            _colors.Remove(colorToDelete);
+        }
+
+        public List<Color> GetAll()
+        {
+            return _colors;
+        }
+
+        public List<Color> GetById(int colorId)
+        {
+            return _colors.Where(p => p.ColorId == colorId).ToList();
+        }
+
+        public void Update(Color color)
+        {
+            Color colorToUpdate = _colors.SingleOrDefault(p => p.ColorId == color.ColorId);
+            colorToUpdate.ColorDescription = color.ColorDescription;
+        }
+    }
+}
