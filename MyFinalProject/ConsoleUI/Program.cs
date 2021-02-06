7d0c7de63fee4ddf6bd7894fe61c4860406dde48
drkamilacar <78367849+drkamilacar@users.noreply.github.com>
2021-02-06 23:55:36 +0300

    06.02.2021 Ders Sonu

--- MyFinalProject/ConsoleUI/Program.cs
+++ MyFinalProject/ConsoleUI/Program.cs
@@ -10,6 +10,22 @@ namespace ConsoleUI
         static void Main(string[] args)
         {
             //ProductTest();
+            //CategoryTest();
+            ProductDetailsTest();
+        }
+
+        private static void ProductDetailsTest()
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal());
+
+            foreach (var product in productManager.GetProductDetails())
+            {
+                Console.WriteLine(product.ProductName + " / " + product.CategoryName);
+            }
+        }
+
+        private static void CategoryTest()
+        {
             CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
             foreach (var category in categoryManager.GetAll())
             {
