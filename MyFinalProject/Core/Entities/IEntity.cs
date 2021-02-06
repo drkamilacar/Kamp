95ed1f5d0d62df03d1bb07d674c4a490ef74c72e
drkamilacar <78367849+drkamilacar@users.noreply.github.com>
2021-02-06 23:24:28 +0300

    06.02.2021 2.Mola

--- MyFinalProject/Core/Entities/IEntity.cs
+++ MyFinalProject/Core/Entities/IEntity.cs
@@ -2,8 +2,9 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Entities.Abstract
+namespace Core.Entities
 {
+    //IEntity iöplement eden class bir veritabanı tablosudur
     public interface IEntity
     {
     }
