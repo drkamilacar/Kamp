1ade09b37cf56e284bf2202ced5bf18e4d31c188
drkamilacar <78367849+drkamilacar@users.noreply.github.com>
2021-02-14 17:19:32 +0300

    14.02.2021 İlk ödev sonu

--- ReCapProject/DataAccess/Concrete/EntityFramework/EfColorDal.cs
+++ ReCapProject/DataAccess/Concrete/EntityFramework/EfColorDal.cs
@@ -10,7 +10,7 @@ using System.Text;
 
 namespace DataAccess.Concrete.EntityFramework
 {
-    class EfColorDal : EfEntityRepositoryBase<Color, ReCapDbContext>, IColorDal
+    public class EfColorDal : EfEntityRepositoryBase<Color, ReCapDbContext>, IColorDal
     {
     }
 }
