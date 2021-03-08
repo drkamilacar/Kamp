c3848fe1fe60570f7bf97048b179601626246f59
drkamilacar <78367849+drkamilacar@users.noreply.github.com>
2021-03-08 12:19:01 +0300

    Ödev 3 sonu

---
+++ ClassMethodDemo/CustomerManager.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMethodDemo
+{
+    public class CustomerManager
+    {
+        List<Customer> _customers;
+
+        public CustomerManager()
+        {
+            _customers = new List<Customer>
+            {
+                new Customer{Id=1,FirstName = "Kamil",LastName="Acar"},
+                new Customer{Id=2,FirstName = "Emine",LastName="Acar"},
+                new Customer{Id=3,FirstName = "Servet Arhan",LastName="Acar"},
+                new Customer{Id=4,FirstName = "Arda Baran",LastName="Acar"},
+            };
+        }
+
+        public void Add(Customer customer)
+        {
+            _customers.Add(customer);
+            Console.WriteLine("*******************************************************************");
+            Console.WriteLine($"Müşteri {customer.FirstName} {customer.LastName} kaydedildi.");
+            Console.WriteLine("*******************************************************************");
+        }
+
+        public void Update(Customer customer)
+        {
+            foreach (var c in _customers)
+            {
+                if (c.Id == customer.Id)
+                {
+                    c.FirstName = customer.FirstName;
+                    c.LastName = customer.LastName;
+                    Console.WriteLine("*******************************************************************");
+                    Console.WriteLine($"Müşteri {customer.FirstName} {customer.LastName} güncellendi.");
+                    Console.WriteLine("*******************************************************************");
+                    return;
+                }
+            }
+        }
+
+        public void Delete(Customer customer)
+        {
+            foreach (var c in _customers)
+            {
+                if (c.Id == customer.Id)
+                {
+                    _customers.Remove(c);
+                    Console.WriteLine("******************************************************************");
+                    Console.WriteLine($"Müşteri {customer.FirstName} {customer.LastName} silindi.");
+                    Console.WriteLine("******************************************************************");
+                    return;
+                }
+            }
+        }
+
+        public List<Customer> GetAll()
+        {
+            Console.WriteLine("************************** MÜŞTERİ LİSTESİ *************************");
+            Console.WriteLine("********************************************************************");
+
+            foreach (var customer in _customers)
+            {
+                Console.WriteLine($"Müşteri No: {customer.Id} | Müşteri Ad Soyad : {customer.FirstName} {customer.LastName}");
+            }
+            Console.WriteLine("********************************************************************");
+            return _customers;
+        }
+    }
+}
---
+++ ClassMethodDemo/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassMethodDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+
+            Console.WriteLine("1. Yeni Müşteri");
+            Console.WriteLine("2. Müşteri Güncelle");
+            Console.WriteLine("3. Müşteri Sil");
+            Console.WriteLine("4. Müşterileri listele");
+            Console.WriteLine("***************************************************");
+
+            var answer = Console.ReadLine();
+
+            if (answer == "1")
+            {
+                Customer customer = new Customer();
+                Console.Write("Id Giriniz: ");
+                customer.Id = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Ad Giriniz: ");
+                customer.FirstName = Console.ReadLine();
+                Console.Write("Soyad Giriniz: ");
+                customer.LastName = Console.ReadLine();
+
+                customerManager.Add(customer);
+                var listOfCustomer = customerManager.GetAll();
+            }
+            else if (answer == "2")
+            {
+                customerManager.GetAll();
+                Customer customer = new Customer();
+
+                Console.Write("Güncellemek istediğiniz müşteriye ait Id Giriniz: ");
+                customer.Id = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Ad Giriniz: ");
+                customer.FirstName = Console.ReadLine();
+                Console.Write("Soyad Giriniz: ");
+                customer.LastName = Console.ReadLine();
+
+                customerManager.Update(customer);
+
+                var listOfCustomer = customerManager.GetAll();
+
+            }
+            else if (answer == "3")
+            {
+                var listOfCustomer = customerManager.GetAll();
+                Customer customer = new Customer();
+
+                Console.Write("Silmek istediğiniz müşteriye ait Id Giriniz: ");
+                customer.Id = Convert.ToInt32(Console.ReadLine());
+                customerManager.Delete(customer);
+
+                listOfCustomer = customerManager.GetAll();
+
+            }
+            else
+            {
+                var listOfCustomer = customerManager.GetAll();
+            }
+        }
+    }
+}
